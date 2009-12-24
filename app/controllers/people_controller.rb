3e0cfb49ffea6aad56932d732717bd62dddbfc09
flov <florian.vallen@gmail.com>
2009-12-24 03:27:18 +0100

    more progress

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,4 +1,8 @@
 class PeopleController < ApplicationController
+  def index
+    
+  end
+
   def new
     @person = Person.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   map.signup 'signup', :controller => 'people', :action => 'new'
   map.logout 'logout', :controller => 'sessions', :action => 'destroy'
   map.login 'login', :controller => 'sessions', :action => 'new'
+  map.welcome 'welcome', :controller => 'people', :action => 'index'
   map.resources :sessions
 
   map.resources :people
