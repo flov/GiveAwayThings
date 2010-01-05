377b45d732e41d5ba652dc5a8708d855ab4b0b3a
flov <florian.vallen@gmail.com>
2010-01-05 21:42:22 +0100

    avatar header to partial

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,10 +2,12 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(params[:item])
+    @item.person=current_person
     if @item.save
       flash[:notice] = "Thank you for giving things away! '#{@item.title}' can now be found by others."
       redirect_to welcome_path
     else
+      flash[:error] = "We are sorry, but the item could not be created."
       redirect_to welcome_path 
     end
   end
