75d7a9888dbb8ce5c470cf94e0391999e909f0e1
flov <florian.vallen@gmail.com>
2010-01-07 21:16:00 +0100

    moved peoples index action to items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,17 @@
 class ItemsController < ApplicationController
 
+  def index
+    @item = Item.new
+  end  
+
   def create
     @item = Item.new(params[:item])
     @item.person=current_person
     if @item.save
       flash[:notice] = "Thank you for giving things away! '#{@item.title}' can now be found by others."
-      redirect_to welcome_path
+      redirect_to items_path
     else
-      redirect_to welcome_path 
+      render :index
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,11 @@ ActionController::Routing::Routes.draw do |map|
   map.signup 'signup',   :controller => 'people',     :action => 'new'
   map.logout 'logout',   :controller => 'sessions',   :action => 'destroy'
   map.login 'login',     :controller => 'sessions',   :action => 'new'
-  map.welcome 'welcome', :controller => 'people',     :action => 'index'
+  map.welcome 'welcome', :controller => 'items',      :action => 'index'
   map.city 'city',       :controller => 'searches',   :action => 'city'
   map.item 'item',       :controller => 'searches',   :action => 'item'
   map.resources :sessions
   map.resources :items
-  map.resources :cities
-
   map.resources :people, :has_many => :items
 
   map.root :controller => "people"
