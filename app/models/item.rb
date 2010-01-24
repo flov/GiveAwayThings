20ceeeccf6681d7ec2aeb6ecc090c7c7cf7dd873
flov <florian.vallen@gmail.com>
2010-01-24 01:51:03 +0100

    images

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ActiveRecord::Base
   belongs_to :address
   belongs_to :category
   has_many :requests
+  has_attached_file :photo
   
   accepts_nested_attributes_for :category
   
--- db/schema.rb
+++ db/schema.rb
@@ -1,56 +1,60 @@
+# This file is auto-generated from the current state of the database. Instead of editing this file, 
+# please use the migrations feature of Active Record to incrementally modify your database, and
+# then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your database schema. If you need
+# to create the application database on another system, you should be using db:schema:load, not running
+# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100123204904) do
+ActiveRecord::Schema.define(:version => 20100123235016) do
 
-  create_table :addresses, :force => true do |t|
-    t.string :street
-    t.string :state
-    t.integer :city_id
+  create_table "addresses", :force => true do |t|
+    t.string  "street"
+    t.string  "state"
+    t.integer "city_id"
   end
 
-  create_table :cities, :force => true do |t|
-    t.string :name
-    t.integer :country_id
-    t.datetime :created_at
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
   end
 
-  add_index :cities, [:name, :country_id], :unique => true
-
-  create_table :countries, :force => true do |t|
-    t.string :name
-    t.string :iso
+  create_table "cities", :force => true do |t|
+    t.string   "name"
+    t.integer  "country_id"
+    t.datetime "created_at"
   end
 
-  create_table :categories, :force => true do |t|
-    t.string   :name
-    t.datetime :created_at
+  add_index "cities", ["name", "country_id"], :name => "index_cities_on_name_and_country_id", :unique => true
+
+  create_table "countries", :force => true do |t|
+    t.string "name"
+    t.string "iso"
   end
 
-  create_table :requests, :force => true do |t|
-    t.integer   :person_id
-    t.integer   :item_id
-    t.string    :title
-    t.text      :text
-    t.boolean   :accepted
-    t.datetime  :created_at
-  end
-  
-  create_table :items, :force => true do |t|
-    t.string   :title
-    t.text     :description
-    t.string   :picture_url
-    t.integer  :taken_by,     :default => 0
-    t.integer  :person_id
-    t.integer  :address_id
-    t.integer  :category_id
-    t.datetime :created_at
-    t.datetime :updated_at
+  create_table "items", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "picture_url"
+    t.integer  "taken_by",           :default => 0
+    t.integer  "person_id"
+    t.integer  "address_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "people", :force => true do |t|
     t.string   "username"
     t.string   "email",          :limit => 100
-    t.integer  :address_id
-    t.boolean  :is_admin
+    t.integer  "address_id"
     t.string   "password_hash"
     t.string   "password_salt"
     t.datetime "created_at"
@@ -58,6 +62,16 @@ ActiveRecord::Schema.define(:version => 20100123204904) do
     t.boolean  "confirmed_user",                :default => false
     t.string   "first_name",     :limit => 20,  :default => ""
     t.string   "last_name",      :limit => 20,  :default => ""
+    t.boolean  "is_admin"
+  end
+
+  create_table "requests", :force => true do |t|
+    t.integer  "person_id"
+    t.integer  "item_id"
+    t.text     "text"
+    t.boolean  "accepted"
+    t.datetime "created_at"
+    t.string   "title"
   end
 
 end
