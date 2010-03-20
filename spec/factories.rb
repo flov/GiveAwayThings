952b8ddd59e67a02afab89212c39b92cd61cc846
flov <florian.vallen@gmail.com>
2010-03-20 13:17:49 +0100

    person factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,25 @@
-Factory.define :user, :class => 'user' do |user|
-  user.login 'flov'
-  user.email 'user@fuckingawesome.com'
-  user.first_name 'User'
-  user.last_name 'Experience'
-  user.password 'makeabarrier'
-  user.password_confirmation 'makeabarrier'
+Factory.sequence :login do |n|
+  "gandhi_#{n}"
+end
+
+Factory.sequence :email do |n|
+  "gandhi_#{n}@localhost.com"
+end
+
+
+Factory.define :person do |person|
+  person.login { Factory.next(:login) }
+  person.email { Factory.next(:email) }
+  person.password 'dragons'
+  person.password_confirmation 'dragons'
+end
+
+Factory.define :flov, :class => 'person' do |person|
+  person.username 'username'
+  person.email 'user@fuckingawesome.com'
+  person.password 'makeabarrier'
+  person.password_confirmation 'makeabarrier'
+  person.city 'Duisburg'
+  person.country 'Germany'
+  person.street 'Darwinstr 50'
 end
