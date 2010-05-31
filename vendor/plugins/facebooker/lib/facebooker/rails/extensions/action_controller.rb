aa6e9a7d6dee31c35a43a281ff2f0c3aab00e74d
flov <florian.vallen@gmail.com>
2010-05-31 09:58:11 +0200

    logout for facebook
    commented out facebook picture

--- vendor/plugins/facebooker/lib/facebooker/rails/extensions/action_controller.rb
+++ vendor/plugins/facebooker/lib/facebooker/rails/extensions/action_controller.rb
@@ -18,30 +18,16 @@ end
 # and also in the post body. We want to ignore the query string ones because they are one
 # request out of date
 # We only do thise when there are POST parameters so that IFrame linkage still works
-if Rails.version < '2.3'
-  class ActionController::AbstractRequest
-    def query_parameters_with_facebooker
-      if request_parameters.blank?
-        query_parameters_without_facebooker
-      else
-        (query_parameters_without_facebooker||{}).reject {|key,value| key.to_s =~ /^fb_sig/}
-      end
+class ActionController::Request
+  def query_parameters_with_facebooker
+    if request_parameters.blank?
+      query_parameters_without_facebooker
+    else
+      (query_parameters_without_facebooker||{}).reject {|key,value| key.to_s =~ /^fb_sig/}
     end
-  
-    alias_method_chain :query_parameters, :facebooker
   end
-else
-  class ActionController::Request
-    def query_parameters_with_facebooker
-      if request_parameters.blank?
-        query_parameters_without_facebooker
-      else
-        (query_parameters_without_facebooker||{}).reject {|key,value| key.to_s =~ /^fb_sig/}
-      end
-    end
-  
-    alias_method_chain :query_parameters, :facebooker
-  end
+
+  alias_method_chain :query_parameters, :facebooker
 end
 
 Mime::Type.register_alias "text/html", :fbml
