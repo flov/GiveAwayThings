2dd8eb8e615b03bb855e8db63300a569f7b8f113
flov <florian.vallen@gmail.com>
2010-06-11 22:28:03 +0200

    added request email and added styled box to profile view

--- app/models/emailer.rb
+++ app/models/emailer.rb
@@ -9,9 +9,15 @@ include ActionView::Helpers::TextHelper
     subject       'Get started with GiveAwayThings!'
     body          :person => person, :login_link => confirm_email_person_url(person.username, :token => person.login_token[0..3])
   end
+
+  def request_email(request)
+    defaults
+    recipients    request.owner.email
+    subject       "[GAT] new request for '#{request.item.title}'"
+    body          :request => request
+  end
   
-  private
-  
+  private  
   def defaults
     content_type  'text/html'
     sent_on       Time.now
